b7175312b19ebe4403150e32eb25e90047ca3647
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2024-01-01 02:20:28 +0600

    user ui

--- app/lib/src/modules/users/users.dart
+++ app/lib/src/modules/users/users.dart
@@ -3,8 +3,35 @@ import 'package:flutter/material.dart';
 class UsersScreen extends StatelessWidget {
   const UsersScreen({super.key});
 
+  get itemBuilder => null;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final t = Theme.of(context);
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 7 / 2),
+      itemBuilder: (context, index) => Padding(
+        padding: const EdgeInsets.all(10),
+        child: MaterialButton(
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          color: t.scaffoldBackgroundColor,
+          onPressed: () {},
+          child: SizedBox(
+            width: double.maxFinite,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('ID : $index'),
+                Text('Name : $index'),
+                Text('Email : $index'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
